acf4802e6af9d2278530710482798ca97d55b740
Roberth <roberthdgg@gmail.com>
2020-04-28 19:33:26 -0400

    Modified build configuration

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>I'm so tired of this shit</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: 'red',
+    fontSize: 30,
+  },
+});
+
+export default App;
